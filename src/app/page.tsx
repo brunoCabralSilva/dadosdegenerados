d2fa0adc78c05873724ea9ecb4bd62d147bdf146
Bruno <bruno.cabral.silva2018@gmail.com>
2025-04-28 09:06:20 -0300

    Conclusão de quem somos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,42 +38,42 @@ export default function Home() {
           <div
             className={`bg-[url(/images/degenerados.jpeg)] bg-center bg-cover h-full sm:h-[40vh] text-white flex relative cursor-pointer border-transparent items-end`}>
             <div className={`absolute w-full h-full bg-black/40`} />
-            <p className="z-10 font-bold text-base sm:text-lg px-3 p-2 relative">Eventos</p>
+            <p className="z-10  text-base sm:text-lg px-3 p-2 relative">Eventos</p>
           </div>
         </Link>
         <Link href="/sessions" className="col-span-1 sm:col-span-2 sm:row-span-4">
           <div
             className={`bg-[url(/images/degenerados3.jpeg)] bg-cover bg-center h-[20vh] sm:h-full text-white flex relative cursor-pointer border-transparent items-end`}>
             <div className={`absolute w-full h-full bg-black/40`} />
-            <p className="z-10 font-bold text-base sm:text-lg px-3 p-2">Sessões</p>
+            <p className="z-10  text-base sm:text-lg px-3 p-2">Sessões</p>
           </div>
         </Link>
         <Link href="/blog" className="col-span-2 sm:col-span-4 sm:row-span-2">
           <div
             className={`bg-[url(/images/degenerados6.jpeg)] bg-cover bg-center h-[20vh] sm:h-[40vh] text-white flex relative cursor-pointer border-transparent items-end`}>
             <div className={`absolute w-full h-full bg-black/40`} />
-            <p className="z-10 font-bold text-base sm:text-lg px-3 p-2">Blog</p>
+            <p className="z-10  text-base sm:text-lg px-3 p-2">Blog</p>
           </div>
         </Link>
         <Link href="/media" className="col-span-1 sm:col-span-2 sm:row-span-2">
           <div
             className={`bg-[url(/images/degenerados7.jpeg)] bg-cover h-[20vh] sm:h-[40vh] text-white flex relative cursor-pointer bg-center border-transparent items-end`}>
             <div className={`absolute w-full h-full bg-black/40`} />
-            <p className="z-10 font-bold text-base sm:text-lg px-3 p-2 relative">Mídia</p>
+            <p className="z-10  text-base sm:text-lg px-3 p-2 relative">Mídia</p>
           </div>
         </Link>
         <Link href="/profile" className="col-span-1 sm:row-span-2 sm:col-span-2">
           <div
             className={`bg-[url(/images/degenerados8.jpeg)] bg-top bg-cover h-[20vh] sm:h-[40vh] text-white flex relative cursor-pointer border-transparent items-end`}>
             <div className={`absolute w-full h-full bg-black/40`} />
-            <p className="z-10 font-bold text-base sm:text-lg px-3 p-2">Perfil</p>
+            <p className="z-10  text-base sm:text-lg px-3 p-2">Perfil</p>
           </div>
         </Link>
         <Link href="/about" className="row-span-2 col-span-2">
           <div
             className={`bg-[url(/images/degenerados4.jpeg)] bg-center bg-cover h-[20vh] sm:h-[40vh] text-white flex relative cursor-pointer border-transparent items-end`}>
             <div className={`absolute w-full h-full bg-black/40`} />
-            <p className="z-10 font-bold text-base sm:text-lg px-3 p-2">Quem Somos</p>
+            <p className="z-10  text-base sm:text-lg px-3 p-2">Quem Somos</p>
           </div>
         </Link>
       </div>
